771cab6a9d2518edc2b129a2f851a6231b3f7405
Merijn <99054535@mydavinci.nl>
2019-11-01 12:14:30 +0100

    nogal wat gelulk en verder gekomen
    het lukt

--- horeca.js
+++ horeca.js
@@ -1,32 +1,42 @@
-//frisdrank:&euro;1,20  bier:&euro;2,00  wijn:&euro;3,00
-var total = 0;
+//frisdrank:&euro;1,20  bier:2,00  wijn:3,00  bitterballen8: 4,00 bitterballen16: 7,00 
+var totalprijs = 0;    
+var bitter = 0;
 
 while(bestel != "stop"){
-    var bestel = prompt("Goededag, welkom bij cafe-lekkernij! Uw keuze bij ons qua dranken zijn: Frisdrank 1,20 euro, bier 2,00 euro en wijn 3,00 euro")
-    var totalpricefris = 0;
-    var totalpricebier = 0;
-    var totalpricewijn = 0;
-    var totalpricebitterbal = 0;   
-        if(bestel != "stop"){ 
-               var aantal = prompt("hoeveel "+ bestel +" wilt u?")
-         
-            if(bestel == "Frisdrank"){
-                kosten = (aantal * 1,20)
-                totalpricefris = totalpricefris + total;
-                document.write("U heeft "+aantal+" frisdrank bestelt en het kost "+"&euro;"+kosten+"<br>")
-            }
-            else if(bestel == "bier"){
-                kosten = (aantal * 1,20)
-                totalpricebier = totalpricebier + total;
-            }
-            else if(bestel == "wijn"){
-                kosten = (aantal * 1,20)
-                totalpricewijn = totalpricewijn + total;
-            }
-            else if(bestel == "stop"){
-            
-            }          
+    var bestel = prompt("Goededag, welkom bij cafe-lekkernij! Uw keuze bij ons qua dranken zijn: frisdrank 1,20 euro, bier 2,00 euro en wijn 3,00 euro")
+    if(bestel != "stop"){
+        var aantal = prompt("hoeveel "+ bestel +" wilt u?")
+        if(bestel == "fris"|| bestel == "frisdrank"){
+           totalprijs += drinken(bestel,aantal,1.2);
         }
-        total = totalpricefris + totalpricebier + totalpricewijn + totalpricebitterbal
-            document.write("Bedankt voor uw bestelling, uw totaal kosten zijn"+"&euro;"+total)
+        else if(bestel == "bier" || bestel == "pils"){
+           totalprijs += drinken(bestel,aantal,2);
+        }
+        else if(bestel == "wijn"){
+           totalprijs += drinken(bestel,aantal,3);
+        }
+    }
+    else if(bestel == "stop"){
+        kosten = totalprijs
+        document.write("Dankuwel voor uw aankoop uw totaal prijs is "+"&euro;"+kosten)
+    }
+    else if(bestel == "snack"){
+        var soort = prompt("hoeveel bitterballen wilt u in een schaal?")
+        if(soort == "8"){
+            bitter = 4;
+        }
+        else if(soort == "16"){
+            bitter = 7;
+        }
+        else{
+            alert("ongeldig graag opnieuw proberen")
+        }
+        
+    }
 }
+function drinken(naam,getal,nummer) {
+    var berekening = getal * nummer
+    document.write("U heeft "+naam+" bestelt met het aantal "+getal+" en de kosten zijn "+"&euro;"+berekening+"<br>")
+    return berekening;
+}
+
